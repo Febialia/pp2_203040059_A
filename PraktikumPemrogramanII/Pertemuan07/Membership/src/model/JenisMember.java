e3b4957e96e4cf192210be831bcd0eea5738be0f
febialia <119504728+Febialia@users.noreply.github.com>
2024-11-04 18:18:54 +0700

    tugas7
    pertemuan ke 7

---
+++ PraktikumPemrogramanII/Pertemuan07/Membership/src/model/JenisMember.java
@@ -0,0 +1,27 @@
+package Pertemuan07.Membership.src.model;
+
+public class JenisMember {
+    private String id;
+    private String nama;
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void addJenisMember(JenisMember jenisMember) {
+  
+        throw new UnsupportedOperationException("Unimplemented method 'addJenisMember'");
+    }
+}
